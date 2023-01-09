e83e2498c2cee5cb907629660302bdfaee6a3e2a
Alexey <alexeylosev1501@gmail.com>
2023-01-09 21:56:10 +0300

    Initial Commit

---
+++ AppleMusic_SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Алексей Лосев on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
