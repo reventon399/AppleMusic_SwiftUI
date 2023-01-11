67c63ea7ae1c78dde26df7a35b8adfb7aa79b216
Alexey <alexeylosev1501@gmail.com>
2023-01-11 22:56:05 +0300

    create view.

---
+++ AppleMusic_SwiftUI/View/Modules/Library/CheckListitemView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckListitemView.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Алексей Лосев on 11.01.2023.
+//
+
+import SwiftUI
+
+struct CheckListitemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckListitemView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckListitemView()
+    }
+}
