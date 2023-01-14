e42820e17e369a45bf93a9cb42a4faf2f1bc0f31
Alexey <alexeylosev1501@gmail.com>
2023-01-14 23:37:08 +0300

    comfigured search bar.

--- AppleMusic_SwiftUI/Views/Modules/Search/SearchBarView.swift
+++ AppleMusic_SwiftUI/Views/Modules/Search/SearchBarView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    @Binding var searchText: String
+    @Binding var isSearchActive: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .font(.system(size: 18))
+                    .foregroundColor(Color(.systemGray3))
+                    .padding(.leading, 12)
+                TextField("Search playlists and tracks", text: $searchText)
+                    .onTapGesture {
+                        isSearchActive = true
+                    }
+                if isSearchActive {
+                    Button(action: {
+                        searchText = ""
+                    }, label: {
+                        Image(systemName: "xmark.circle.fill")
+                    })
+                    .foregroundColor(Color(.systemGray3))
+                    .padding(.trailing, 12)
+                }
+            }
+            .frame(height: 40)
+            .background(Color.init(.systemGray6))
+            .cornerRadius(15)
+            .padding(.top, 5)
+            .padding([.leading, .trailing], 15)
+            
+            if isSearchActive {
+                Button(action: {
+                    searchText = ""
+                    isSearchActive = false
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                                              to: nil,
+                                                              from: nil,
+                                                              for: nil)
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                })
+                .padding(.trailing, 15)
+            }
+        }
+        .transition(.move(edge: .trailing))
+        .animation(.linear, value: 0.1)
     }
 }
 
-struct SearchBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchBarView()
-    }
-}
