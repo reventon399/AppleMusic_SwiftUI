23c08425c7d5e5c4c99a34324f5636f83c9da0a8
Alexey <alexeylosev1501@gmail.com>
2023-01-16 01:43:53 +0300

    create UIKit files.

---
+++ AppleMusic_SwiftUI/Views/Modules/Search/UIKit/CategoryCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryCollectionViewCell.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Алексей Лосев on 16.01.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AppleMusic_SwiftUI/Views/Modules/Search/UIKit/CategoryHeader.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryHeader.swift
+//  AppleMusic_SwiftUI
+//
+//  Created by Алексей Лосев on 16.01.2023.
+//
+
+import UIKit
+
+class CategoryHeader: UICollectionReusableView {
+        
+}
