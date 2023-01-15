76478d45304e18b0129ba19ed0d45cfc64f3eaca
Alexey <alexeylosev1501@gmail.com>
2023-01-16 02:17:55 +0300

    changed header.

--- AppleMusic_SwiftUI/Views/Modules/Search/UIKit/CategoryHeader.swift
+++ AppleMusic_SwiftUI/Views/Modules/Search/UIKit/CategoryHeader.swift
@@ -8,5 +8,46 @@
 import UIKit
 
 class CategoryHeader: UICollectionReusableView {
+    
         static var identifier = "CategoryHeader"
+
+    // MARK: - Outlets
+
+    private lazy var headerTitle: UILabel = {
+        let header = UILabel()
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.text = Constants.Strings.SectionHeaders.categoryListHeader
+        header.textAlignment = .left
+        header.textColor = .black
+        header.font = .systemFont(ofSize: 22, weight: .bold)
+        return header
+    }()
+
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupHeirarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    // MARK: - Setup
+
+    private func setupHeirarchy() {
+        addSubview(headerTitle)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            headerTitle.topAnchor.constraint(equalTo: self.topAnchor),
+            headerTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            headerTitle.widthAnchor.constraint(equalTo: self.widthAnchor),
+            headerTitle.heightAnchor.constraint(equalTo: self.heightAnchor)
+        ])
+    }
 }
